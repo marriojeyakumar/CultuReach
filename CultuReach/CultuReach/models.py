fe1a64a98872ec851e4d4d77ac3d6844115f4835
Dylan Zickus <dzickus2024@student.amsacs.org>
2024-02-11 09:48:13 -0500

    Co-authored-by: drewcod <drewcod@users.noreply.github.com> gotch

--- CultuReach/CultuReach/models.py
+++ CultuReach/CultuReach/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 
-class Event(models.Model):
+class event(models.Model):
     event_id = models.IntegerField()
-    name = models.CharField(max_length=45)
-    
+    name = models.CharField(45)
+    host_id = models.CharField(45)
+    start_time = models.IntegerField()
+    location = models.CharField(45)
+    tags = models.CharField(45)
+    culture = models.CharField(45)
+
+class users(models.Model):
+    user_id = models.IntegerField()
+    email = models.CharField(45)
+    pswrd = models.CharField(45)
+    username = models.CharField(45)
+
+class roster(models.Model):
+    event_id = models.IntegerField()
+    user_id = models.IntegerField()
+
+class forum(models.Model):
+    user_id = models.IntegerField()
+    message = models.CharField(max_length=225)
+    event_id = models.IntegerField()
+    time = models.IntegerField()
--- CultuReach/CultuReach/views.py
+++ CultuReach/CultuReach/views.py
@@ -1,7 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import *
 
-def index(request):
-    return HttpResponse("Goodbye, world.")
+def search(request):
+    context = {}
 
-def record_detail(request, record_id):
-    return HttpResponse("Hello, world. You're record is: "+str(record_id))
+    if request.method == 'GET':
+        culture = request.GET.get('event_culture')
+        tags = request.GET.get('event_tags')
+        tags = tags.split()
+
+        events = event.objects.filter(event_culture=culture)
+
+        for event1 in events:
+            if event1.tags
+
+
+        
+    return render(request, 'index.html', context)
+
+def createEvent(request, host_id, start_time, name, location, description, tags, culture):
+    event = Event(host_id, start_time, )
