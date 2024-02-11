c3ce6d5938757057c83c3ba4b42befaad99311eb
Manraaj Singh <manraajsingh2006@gmail.com>
2024-02-11 14:10:54 -0500

    d

---
+++ CultuReach/frontend/src/App.js
@@ -0,0 +1,39 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import AppBar from './components/AppBar';
+import HomeHeader from './components/HomeHeader';
+import CultureSelector from './components/CultureSelector';
+import EventsDisplay from './components/EventsDisplay';
+import EventTypeSelector from './components/EventTypeSelector';
+import ContactForm from './components/ContactForm';
+import AboutUs from './components/AboutUs'; 
+import EventsPage from './EventsPage';
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <AppBar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <HomeHeader />
+              <CultureSelector />
+              <EventTypeSelector />
+              <EventsDisplay />
+              <ContactForm />
+            </>
+          } />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/events" element={<EventsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
